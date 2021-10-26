2cb8828ee658e4813ce555924ae25627e83c3d12
victor <victor.cotescu@yahoo.com>
2021-10-27 00:49:21 +0300

    general style improved

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,25 @@
-var slideIndex = 0;
-showSlides();
+let slideIndex = 0;
+callByPos()
+
+function callByPos(){
+    if(window.scrollY < 1000)
+        showSlides()
+    else
+        callByPos()
+}
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+        let i;
+        const slides = document.getElementsByClassName("prez_element");
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(callByPos, 5000);
 }
