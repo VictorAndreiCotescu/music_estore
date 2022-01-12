13b68f2eb402e65c925d4dfaf42f257595b63ed4
victor <victor.cotescu@yahoo.com>
2022-01-12 19:17:16 +0200

    fixed missing listener
    Took 7 hours 9 minutes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const date = new Date()
 let app = express()
 
 let client = new Client({
-    user:       'victor2',
+    user:       'victor',
     password:   'parola123',
     host:       'localhost',
     port:       5432,
@@ -18,11 +18,14 @@ let client = new Client({
 });
 
 client.connect();
-client.query("select * from produse");
-console.log(client._connected);
+client.query("select * from produse", function (err, rez) {
+    // console.log(err);
+    // console.log(rez);
+});
 
 
-app.set("view engine", "ejs")
+
+app.set("view engine", "ejs");
 
 app.use("/res", express.static(__dirname + "/res"))
 app.use("/css", express.static(__dirname + "/css"))
@@ -30,7 +33,38 @@ app.use("/js", express.static(__dirname + "/js"))
 
 
 
+app.get(["/", "/index", "/home"], function (req, res) {
 
+    console.log(req.url)
+    console.log(req.ip)
+
+    let buf = fs.readFileSync(__dirname + "/res/json/galerie.json").toString("utf-8")
+    let ob_img = JSON.parse(buf)
+
+    for (let imag of ob_img.imagini) {
+        let nume_imag = imag.cale_fisier.split(".")[0]
+
+        let dim_mic = 150
+        let dim_mediu = 300
+        let dim_mare = 500
+
+        imag.mic = `${ob_img.cale_galerie}/mic/${nume_imag}.webp`
+        // imag.mediu = `${ob_img.cale_galerie}/mediu/${nume_imag}.webp`
+        // imag.mare = `${ob_img.cale_galerie}/mare/${nume_imag}.webp`
+
+        imag.ini = `${ob_img.cale_galerie}/${imag.cale_fisier}`
+
+        // if (!fs.existsSync(imag.mic))
+        //     sharp(__dirname + "/" + imag.ini).resize(dim_mic).toFile(__dirname + "/" + imag.mic)
+        // if (!fs.existsSync(imag.mediu))
+        //     sharp(__dirname + "/" + imag.ini).resize(dim_mediu).toFile(__dirname + "/" + imag.mediu)
+        // if (!fs.existsSync(imag.mare))
+        //     sharp(__dirname + "/" + imag.ini).resize(dim_mare).toFile(__dirname + "/" + imag.mare)
+
+    }
+    console.log("intra");
+    res.render("pagini/index", {ip: req.ip, imagini: ob_img.imagini, cale: ob_img.cale_galerie, data: date.getMonth()})
+})
 
 app.get("/produse", function(req, res){
 
@@ -59,37 +93,7 @@ app.get("/produse", function(req, res){
     });
 })
 
-app.get(["/", "/index", "/home"], function (req, res) {
-    console.log(req.url)
-    console.log(req.ip)
 
-    let buf = fs.readFileSync(__dirname + "/res/json/galerie.json").toString("utf-8")
-    let ob_img = JSON.parse(buf)
-
-    for (let imag of ob_img.imagini) {
-        let nume_imag = imag.cale_fisier.split(".")[0]
-
-        let dim_mic = 150
-        let dim_mediu = 300
-        let dim_mare = 500
-
-        imag.mic = `${ob_img.cale_galerie}/mic/${nume_imag}.webp`
-        // imag.mediu = `${ob_img.cale_galerie}/mediu/${nume_imag}.webp`
-        // imag.mare = `${ob_img.cale_galerie}/mare/${nume_imag}.webp`
-
-        imag.ini = `${ob_img.cale_galerie}/${imag.cale_fisier}`
-
-        // if (!fs.existsSync(imag.mic))
-        //     sharp(__dirname + "/" + imag.ini).resize(dim_mic).toFile(__dirname + "/" + imag.mic)
-        // if (!fs.existsSync(imag.mediu))
-        //     sharp(__dirname + "/" + imag.ini).resize(dim_mediu).toFile(__dirname + "/" + imag.mediu)
-        // if (!fs.existsSync(imag.mare))
-        //     sharp(__dirname + "/" + imag.ini).resize(dim_mare).toFile(__dirname + "/" + imag.mare)
-
-    }
-
-    res.render("pagini/index", {ip: req.ip, imagini: ob_img.imagini, cale: ob_img.cale_galerie, data: date.getMonth()})
-})
 
 app.get('/*.ejs', function (req, res) {
         res.status(403).render("pagini/403")
@@ -108,3 +112,4 @@ app.get("/*", function (req, res) {
     )
 })
 
+app.listen(8080);
